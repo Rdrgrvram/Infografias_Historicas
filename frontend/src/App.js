412e9ac8b0e1355c04082eb11642f30b4f615c68
Rdrgrvram <144700951+Rdrgrvram@users.noreply.github.com>
2025-04-15 00:14:25 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,25 @@
-import React from 'react';
+// src/App.js
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import InfografiaGenerator from "./pages/InfografiaGenerator";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
-  return <div>Generador de Infografías Históricas</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/generador" element={<InfografiaGenerator />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
+
 export default App;
